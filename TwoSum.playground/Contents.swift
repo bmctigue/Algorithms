d67702478b354c8bf4dc6dcf845868b9f7e6687b
Bruce McTigue <bmctigue@tiguer.com>
2016-03-06 14:30:29 -0700

    Updates to 2 search using my algorithm and common hash solution.

--- TwoSum.playground/Contents.swift
+++ TwoSum.playground/Contents.swift
@@ -1,8 +1,11 @@
 import UIKit
 
 func twoSum(intArray:[Int], target:Int) -> [Int] {
+    if intArray.count < 2 {
+        return []
+    }
     let indexArray = findIndexsForTwoSum(intArray, target:target)
-    if indexArray.count == 0 {
+    if indexArray.count < 2 {
         return []
     }
     let resultArray = formatResult(indexArray)
@@ -25,19 +28,46 @@ func twoSum(intArray:[Int], target:Int) -> [Int] {
 //    return indexArray;
 //}
 
-// this is the good way, check if array contains the combinations that add up to target, O(Cn)
+// this is one way, check if array contains the combinations that add up to target, O(Cn), this one is good if target/2 > size of input array
+//func findIndexsForTwoSum(intArray:[Int], target:Int) -> [Int] {
+//    var targetMinusIndex:Int
+//    for i in 0..<target {
+//        targetMinusIndex = target-i
+//        if  (i >= targetMinusIndex) {
+//            printSeparator()
+//            print ("no match")
+//            break
+//        }
+//        print("i:\(i) targetMinusIndex:\(targetMinusIndex)")
+//        if let index1 = intArray.indexOf(i) {
+//            if let index2 = intArray.indexOf(targetMinusIndex) {
+//                printSeparator()
+//                return [index1,index2]
+//            }
+//        }
+//    }
+//    printSeparator()
+//    return []
+//}
+
+// also good, create hash of values and return if correct partner found
 func findIndexsForTwoSum(intArray:[Int], target:Int) -> [Int] {
-    var targetMinusIndex:Int
+    var targetPartner:Int
+    var hashTable:[Int:Int] = [:]
     for i in 0..<intArray.count {
-        targetMinusIndex = target-i
-        if let index1 = intArray.indexOf(i) {
-            if let index2 = intArray.indexOf(targetMinusIndex) {
-                if index1 != index2 {
-                    return [index1,index2]
-                }
+        hashTable[intArray[i]] = i
+        targetPartner = target-intArray[i]
+        print("i:\(i) targetPartner:\(targetPartner)")
+        if let index2 = hashTable[targetPartner] {
+            if i != index2 {
+                printSeparator()
+                return [i,index2]
             }
         }
     }
+    printSeparator()
+    print ("no match")
+    printSeparator()
     return []
 }
 
@@ -45,6 +75,24 @@ func formatResult(indexArray:[Int]) -> [Int] {
     var indexArray = [indexArray[0],indexArray[1]]
     indexArray = indexArray.sort()
     return [indexArray[0]+1,indexArray[1]+1]
+}
+
+func printSeparator() {
+    print ("============")
+}
+
+func testTwoSumArrayCenterCheckOdd() {
+    let inputArray = [7,2,1,3,5]
+    let correctArray = []
+    let resultArray = twoSum(inputArray, target:13)
+    resultArray == correctArray
+}
+
+func testTwoSumArrayCenterCheckEven() {
+    let inputArray = [7,2,1,3,6]
+    let correctArray = []
+    let resultArray = twoSum(inputArray, target:12)
+    resultArray == correctArray
 }
 
 func testTwoSumArrayFaultyInput() {
@@ -82,6 +130,27 @@ func testTwoSumArrayTarget2() {
     resultArray == correctArray
 }
 
+func testTwoSumArrayTarget2NoMatch() {
+    let inputArray = [0,1]
+    let correctArray = []
+    let resultArray = twoSum(inputArray, target:2)
+    resultArray == correctArray
+}
+
+func testTwoSumArrayTarget1() {
+    let inputArray = [0,1]
+    let correctArray = [1,2]
+    let resultArray = twoSum(inputArray, target:1)
+    resultArray == correctArray
+}
+
+func testTwoSumArraySize1() {
+    let inputArray = [0]
+    let correctArray = []
+    let resultArray = twoSum(inputArray, target:2)
+    resultArray == correctArray
+}
+
 func testFormatResultInOrder() {
     let index1 = 1
     let index2 = 2
@@ -100,8 +169,13 @@ func testFormatResultInOutOrder() {
 
 testFormatResultInOrder()
 testFormatResultInOutOrder()
+testTwoSumArraySize1()
+testTwoSumArrayTarget1()
 testTwoSumArrayTarget2()
+testTwoSumArrayTarget2NoMatch()
 testTwoSumArrayTarget4()
 testTwoSumArrayTarget6()
 testTwoSumArrayTarget7()
 testTwoSumArrayFaultyInput()
+testTwoSumArrayCenterCheckEven()
+testTwoSumArrayCenterCheckOdd()
