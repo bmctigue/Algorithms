84d560af999a49c0fab78e98c1383a4020f9a376
Bruce McTigue <bmctigue@tiguer.com>
2016-03-27 11:42:26 -0700

    Updated the code to handle dequeuing better so is O(1), not O(n)

--- Queue.playground/Contents.swift
+++ Queue.playground/Contents.swift
@@ -9,34 +9,58 @@ enum Dish {
 }
 
 struct QueueOfDishes {
-    var dishes:[Dish] = []
+    var dishes:[Dish?] = []
+    var head = 0
+    
+    var isEmpty: Bool {
+        return self.count() == 0
+    }
+    
+    func count() -> Int {
+        return dishes.count - head
+    }
     
     mutating func enqueue(dish:Dish) {
         dishes.append(dish)
     }
     
     mutating func dequeue() -> Dish? {
-        if (count() == 0) {
+        if (self.isEmpty) {
             return nil
         }
-        return dishes.removeFirst()
-    }
-    
-    func count() -> Int {
-        return dishes.count
+        let dish = dishes[head]
+        dishes[head] = nil
+        head += 1
+        
+        let percentage = Double(head)/Double(dishes.count)
+        if dishes.count > 50 && percentage > 0.25 {
+            dishes.removeFirst(head)
+            head = 0
+        }
+        
+        return dish
     }
     
     mutating func clear() {
-        var dish: Dish?
-        repeat {
-            dish = dequeue()
-        } while dish != nil
+        dishes = []
+        head = 0
     }
     
     func printQueue() {
         for dish in dishes {
             print("\(dish)")
         }
+    }
+    
+    func printHead() {
+        print("head: \(head)")
+    }
+    
+    func printData() {
+        print("----Data-----")
+        self.printHead()
+        self.printQueue()
+        print("-------------")
     }
 }
 
@@ -52,25 +76,24 @@ func testQueue() {
     assert(testDish == nil, "Dish dequeued from empty queue is not nil")
     print("Dequeueing from empty queue returns nil")
     print("-------------")
-    
+
     queue.clear()
     queue.enqueue(dinnerDish)
     assert(queue.count() == 1, "queue size is not 1")
-    queue.printQueue()
-    print("-------------")
+    queue.printData()
     
     queue.clear()
     queue.enqueue(dinnerDish)
     queue.enqueue(saladDish)
     queue.enqueue(breadDish)
+    queue.printData()
     if let dish = queue.dequeue() {
         assert(dish == dinnerDish)
         print("The dequeued dish is a dinner dish")
     }
     assert(queue.count() == 2, "queue size is not 2")
-    queue.printQueue()
-    print("-------------")
-    
+    queue.printData()
+
     queue.clear()
     queue.enqueue(dinnerDish)
     queue.enqueue(saladDish)
@@ -78,6 +101,37 @@ func testQueue() {
     queue.clear()
     assert(queue.count() == 0, "queue size is not 0")
     print("queue is empty")
+    print("-------------")
+    
+    queue.clear()
+    queue.enqueue(dinnerDish)
+    queue.enqueue(saladDish)
+    queue.enqueue(breadDish)
+    queue.enqueue(dinnerDish)
+    queue.printData()
+    if let dish1 = queue.dequeue() {
+        assert(dish1 == dinnerDish)
+        print("The dequeued dish is a dinner dish")
+    }
+    queue.printData()
+    if let dish2 = queue.dequeue() {
+        assert(dish2 == saladDish)
+        print("The dequeued dish is a salad dish")
+    }
+    queue.printData()
+    if let dish3 = queue.dequeue() {
+        assert(dish3 == breadDish)
+        print("The dequeued dish is a bread dish")
+    }
+    queue.printData()
+    if let dish4 = queue.dequeue() {
+        assert(dish4 == dinnerDish)
+        print("The dequeued dish is a dinner dish")
+    }
+    queue.printData()
+    print("queue.count: \(queue.count())")
+    assert(queue.count() == 0, "queue size is not 0")
+    assert(queue.isEmpty,"The queue is not empty")
     print("-------------")
     
     queue.clear()
