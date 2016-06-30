ffff5c304d10f058df04e4e01b1f7c269beb1c7b
Bruce McTigue <bmctigue@tiguer.com>
2016-06-29 18:49:59 -0600

    String algorithm update

--- Strings.playground/Contents.swift
+++ Strings.playground/Contents.swift
@@ -9,6 +9,10 @@ func reverseString(testString: String) -> String {
     var lower:Int = 0
     var upper:Int = chars.count - 1
     var holder: Character
+//    print("\(chars)")
+    if chars.count < 2 {
+        return testString
+    }
     
     while lower < upper {
         holder = chars[upper]
@@ -23,25 +27,21 @@ func reverseString(testString: String) -> String {
 //print("\(reverseString(str))")
 
 str = "My name is X Y Z"
+//str = "a b c d e f"
+//str = "abcdef"
 
 func reverseWordsInString(testString:String, delimiter:Character) -> String {
-    let appendedString = testString + String(delimiter)
+    let appendedString = String(delimiter) + testString + String(delimiter)
     var chars = Array(appendedString.characters)
-    var subChars: Array<Character>
     var wordStart:Int = 0
     var prevChar: Character = " "
     var currentChar:Character
-    var reversedWord:String
     
     for index in 0..<chars.count {
         currentChar = chars[index]
         if currentChar == delimiter {
-            if prevChar != delimiter {
-                reversedWord = reverseString(appendedString[appendedString.startIndex.advancedBy(wordStart)...appendedString.startIndex.advancedBy(index)])
-                subChars = Array(reversedWord.characters)
-                for subIndex in 0..<subChars.count {
-                    chars[subIndex + wordStart] = subChars[subIndex]
-                }
+            if prevChar != delimiter && (index - wordStart) > 1 {
+                chars = updateCharsWithReversedWord(chars, appendedString: appendedString, wordStart: wordStart, index: index)
             }
         } else {
             if prevChar == delimiter {
@@ -51,10 +51,22 @@ func reverseWordsInString(testString:String, delimiter:Character) -> String {
         prevChar = currentChar
     }
     var reversedString = String(chars)
-    reversedString = reversedString[reversedString.startIndex.successor()..<reversedString.endIndex]
+    reversedString = reversedString[reversedString.startIndex.successor()..<reversedString.endIndex.predecessor()]
     return reverseString(reversedString)
+}
+
+func updateCharsWithReversedWord(chars:Array<Character>, appendedString:String, wordStart:Int, index:Int) -> Array<Character> {
+    var newChars = chars
+    let reversedWord = reverseString(appendedString[appendedString.startIndex.advancedBy(wordStart)...appendedString.startIndex.advancedBy(index-1)])
+    let subChars = Array(reversedWord.characters)
+    for subIndex in 0..<subChars.count {
+        newChars[subIndex + wordStart] = subChars[subIndex]
+    }
+    return newChars
 }
 
 let delimiter: Character = " "
 let result = reverseWordsInString(str, delimiter: delimiter)
 print("\(result)")
+
+
