3758735f0380d4457f32babb47143e8f231dfe74
Bruce McTigue <bmctigue@tiguer.com>
2016-07-01 10:15:49 -0600

    Added bubble sort and insertion sort examples.

--- Arrays.playground/Contents.swift
+++ Arrays.playground/Contents.swift
@@ -2,7 +2,69 @@
 
 import UIKit
 
-var x = 0
+// bubble sort
+
+//let sourceArray: Array<Int> = []
+//let sourceArray: Array<Int> = [3]
+//let sourceArray: Array<Int> = [0,3]
+//let sourceArray: Array<Int> = [3,0]
+let sourceArray: Array<Int> = [3,66,7,1,9,0]
+//let sourceArray: Array<Int> = [1,2,3,4,5,6]
+//let sourceArray: Array<Int> = [6,5,4,3,2,1]
+
+func bubbleSortForArray(testArray:Array<Int>) -> Array<Int> {
+    if testArray.count < 2 {
+        return testArray
+    }
+    var bubbleArray:Array<Int> = testArray
+    let endIndex = bubbleArray.count-2
+    var temp: Int
+    var swapsPerformed = true
+    while swapsPerformed {
+        swapsPerformed = false
+        for index in 0...endIndex {
+            if bubbleArray[index] > bubbleArray[index+1] {
+                temp = bubbleArray[index+1]
+                bubbleArray[index+1] = bubbleArray[index]
+                bubbleArray[index] = temp
+                swapsPerformed = true
+            }
+        }
+    }
+    return bubbleArray
+}
+
+let bubbleResult = bubbleSortForArray(sourceArray)
+
+// insertion sort
+
+func insertionSortForArray(testArray:Array<Int>) -> Array<Int> {
+    if testArray.count < 2 {
+        return testArray
+    }
+    var insertionArray:Array<Int> = testArray
+    let endIndex: Int = insertionArray.count-1
+    var temp: Int
+    var insertionIndex: Int = endIndex
+    for _ in 0..<endIndex {
+        // add last item in unsorted portion to the sorted portion
+        insertionIndex -= 1
+        // sort the unsorted portion
+        for sortIndex in insertionIndex..<endIndex {
+            if insertionArray[sortIndex] <= insertionArray[sortIndex+1] {
+                break
+            } else {
+                temp = insertionArray[sortIndex+1]
+                insertionArray[sortIndex+1] = insertionArray[sortIndex]
+                insertionArray[sortIndex] = temp
+            }
+        }
+    }
+    
+    return insertionArray
+}
+
+let insertionResult = insertionSortForArray(sourceArray)
 
 var array1 = [2,3,1,0,2,5,3]
 
@@ -68,6 +130,7 @@ func checkIfIncluded(testArray:Array<Int>, test: Int) -> Bool {
     }
 }
 
+// binary search
 func searchArray(testArray:Array<Int>, test: Int) -> Bool {
     let index:Int = testArray.count / 2
     let indexValue = testArray[index]
