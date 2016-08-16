ed21cd04e07e7cdb1b246cf8e1e509417ccb5177
Bruce McTigue <bmctigue@tiguer.com>
2016-08-16 15:17:20 -0600

    Added a new linked list playground

---
+++ LinkedList.playground/Contents.swift
@@ -0,0 +1,54 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+
+protocol Node {
+    var data: Int { get set }
+    var nextNode: Node? { get set }
+}
+
+struct ListNode: Node {
+    var data: Int
+    var nextNode: Node?
+    init(data: Int) {
+        self.data = data
+    }
+}
+
+protocol Linked {
+    var head: Node? { get set }
+    mutating func insertHead(node: Node)
+}
+
+struct LinkedList: Linked {
+    var head: Node?
+    
+    mutating func insertHead(node: Node) {
+        var node = node
+        if let head = self.head {
+            node.nextNode = head
+        }
+        self.head = node
+    }
+    
+    func printList() {
+        var node: Node? = head
+        var nodeList: String = "["
+        while node != nil {
+            nodeList += "\(node!.data)"
+            node = node!.nextNode
+            if (node != nil) {
+               nodeList += ", "
+            }
+        }
+        nodeList += "]"
+        print(nodeList)
+    }
+}
+
+let node = ListNode(data: 5)
+let node2 = ListNode(data: 2)
+var list = LinkedList()
+list.insertHead(node)
+list.insertHead(node2)
+list.printList()
